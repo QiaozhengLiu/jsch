1dccf571bc9de4ca05f33caa0f23e4a3c35c3689
Qiaozheng Liu <qiaozheng.liu@pax.us>
2024-02-20 11:49:19 -0500

    first commit, restore to original v0.1.55, migrate to gradle project

---
+++ jsch/src/main/java/com/jcraft/jsch/jce/ARCFOUR128.java
@@ -0,0 +1,11 @@
+/*
+This class raises risks in SAST report.
+This class is not used in our project.
+The solution is to remove all content of the class and keep an empty file. 
+ */
+
+ package com.jcraft.jsch.jce;
+
+ public class ARCFOUR128 {
+ 
+ }
